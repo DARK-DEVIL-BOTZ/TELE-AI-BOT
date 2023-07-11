c7ed62fa8710fa34da45e2f6b0cccf916c904249
TEAM DARK DEVIL <111672563+DARK-DEVIL-BOTZ@users.noreply.github.com>
2023-07-11 22:43:41 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+## CONFIGURATION FILE
+
+# Bot session name
+session_name_bot = "sessions/bot" 
+
+# Follow this guide to obtain your API credentials: 
+# https://core.telegram.org/api/obtaining_api_id
+API_ID = 19562731 
+API_HASH = "1667473e722f17eaa8ccbb98dc5f727d"
+
+# Write to @BotFather in Telegram to create a bot token
+BOT_TOKEN = "6086029944:AAEZfp7PbmFahcCwb7-qelmZAsb56jaAT7I"
+
+# The ID of the OpenAI model to use for generating responses.
+model_engine = "gpt-3.5-turbo"
+
+# Get your API_KEY by creating an OpenAi account and the go here: 
+# https://platform.openai.com/account/api-keys
+openai_key = "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"
